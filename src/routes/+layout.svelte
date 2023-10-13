81974e4b3669763d95edf049c5139a93948a6247
Ryan Sheehan <ryansheehan@pop-os.localdomain>
2023-10-13 17:35:22 -0500

    install sass and postcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import "$lib/sass/global.scss";
+</script>
+
+<slot/>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import postcssPresetEnv from 'postcss-preset-env';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `				
+			
+				`,
+			}
+		},
+		postcss: {
+			plugins: [
+				postcssPresetEnv({
+					stage: 3,
+				}),
+			]
+		},
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
